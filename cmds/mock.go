143884e0c68138ab8116129d4b796972443d9946
mu8086 <mu8086@gmail.com>
2024-03-10 12:00:14 +0800

    chore: add mock cmd

---
+++ cmds/mock.go
@@ -0,0 +1,56 @@
+package cmds
+
+import (
+	"VirtualFileSystem/errors"
+	"fmt"
+	"os"
+)
+
+func init() {
+	//register(Mock{})
+}
+
+type Mock struct{}
+
+func (cmd Mock) Execute(args []string) error {
+	if err := cmd.validate(args); err != nil {
+		return err
+	}
+
+	// do something
+	arg0, arg1 := args[0], args[1]
+	fmt.Fprintf(os.Stdout, "arg0: %v, arg1: %v\n", arg0, arg1)
+
+	fmt.Fprintf(os.Stdout, "Execute %v successfully.\n", cmd.Name())
+	return nil
+}
+
+func (cmd Mock) Name() string {
+	return "MockCmd"
+}
+
+func (cmd Mock) String() string {
+	return fmt.Sprintf("[%s]", cmd.Name())
+}
+
+func (cmd Mock) Usage() {
+	fmt.Fprintf(os.Stderr, "Usage: %v [username] [foldername]\n", cmd.Name())
+}
+
+func (cmd Mock) validate(args []string) error {
+	if len(args) != 2 {
+		cmd.Usage()
+		return errors.ErrArgSize
+	}
+
+	arg0, arg1 := args[0], args[1]
+	fmt.Fprintf(os.Stdout, "arg0: %v, arg1: %v\n", arg0, arg1)
+
+	// validate fields:
+	//
+	// if err != nil {
+	//     return err
+	// }
+
+	return nil
+}
